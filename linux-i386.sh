e40f4d250cc2018eb4ced9e80884d27c7aeba4d6
Ryuichi Sai <ryuichi@ryuichisai.com>
2019-09-25 21:20:27 -0500

    Try docker to build linux-i386

---
+++ linux-i386.sh
@@ -0,0 +1,9 @@
+wget http://releases.llvm.org/9.0.0/llvm-9.0.0.src.tar.xz
+tar xf llvm-9.0.0.src.tar.xz
+mkdir llvm-9.0.0.build
+cd llvm-9.0.0.build/
+cmake -DLLVM_BUILD_LLVM_DYLIB=ON -DCMAKE_INSTALL_PREFIX=../llvm-9.0.0-linux-i386 -DCMAKE_BUILD_TYPE=Release ../llvm-9.0.0.src
+make -j2
+make install
+cd ..
+tar czf llvm-9.0.0-linux-i386.tar.gz llvm-9.0.0-linux-i386
