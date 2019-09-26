0d651ec62e2027a54bc792cebb6d1151ead32063
Ryuichi Sai <ryuichi@ryuichisai.com>
2019-09-26 09:55:46 -0500

    Disable including tests on windows

--- windows-x86_64.bat
+++ windows-x86_64.bat
@@ -2,7 +2,7 @@ curl -L http://releases.llvm.org/9.0.0/llvm-9.0.0.src.tar.xz -o llvm-9.0.0.src.t
 tar xf llvm-9.0.0.src.tar.xz
 mkdir llvm-9.0.0.build
 cd llvm-9.0.0.build
-cmake -G"MinGW Makefiles" -DLLVM_BUILD_LLVM_DYLIB=ON -DCMAKE_INSTALL_PREFIX=..\llvm-9.0.0-windows-x86_64 -DCMAKE_BUILD_TYPE=Release ..\llvm-9.0.0.src
+cmake -G"MinGW Makefiles" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_INCLUDE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=..\llvm-9.0.0-windows-x86_64 -DCMAKE_BUILD_TYPE=Release ..\llvm-9.0.0.src
 mingw32-make -j2
 mingw32-make install
 cd ..
