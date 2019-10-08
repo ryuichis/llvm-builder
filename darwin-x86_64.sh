ea3e24ae45596e5870a7e78dcea33c10fd48cf27
Ryuichi Sai <ryuichi@ryuichisai.com>
2019-10-07 20:34:10 -0500

    Try build llvm-8

--- darwin-x86_64.sh
+++ darwin-x86_64.sh
@@ -1,9 +1,9 @@
-wget http://releases.llvm.org/9.0.0/llvm-9.0.0.src.tar.xz
-tar xf llvm-9.0.0.src.tar.xz
-mkdir llvm-9.0.0.build
-cd llvm-9.0.0.build/
-cmake -DLLVM_BUILD_LLVM_DYLIB=ON -DCMAKE_INSTALL_PREFIX=../llvm-9.0.0-darwin-x86_64 -DCMAKE_BUILD_TYPE=Release ../llvm-9.0.0.src
+wget http://releases.llvm.org/8.0.1/llvm-8.0.1.src.tar.xz
+tar xf llvm-8.0.1.src.tar.xz
+mkdir llvm-8.0.1.build
+cd llvm-8.0.1.build/
+cmake -DLLVM_BUILD_LLVM_DYLIB=ON -DCMAKE_INSTALL_PREFIX=../llvm-8.0.1-darwin-x86_64 -DCMAKE_BUILD_TYPE=Release ../llvm-8.0.1.src
 make -j2
 make install
 cd ..
-tar czf llvm-9.0.0-darwin-x86_64.tar.gz llvm-9.0.0-darwin-x86_64
+tar czf llvm-8.0.1-darwin-x86_64.tar.gz llvm-8.0.1-darwin-x86_64
