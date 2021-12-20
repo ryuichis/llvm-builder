4de8e2ff141b15009e03fe437427039109003b43
Ryuichi Sai <ryuichi@ryuichisai.com>
2021-12-20 09:11:49 -0600

    Build llvm 13.0.0 release build

---
+++ build-release.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+git checkout llvmorg-13.0.0
+cd ..
+mkdir llvm-build
+cd llvm-build/
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../llvm-release -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm-project/llvm
+make -j4
+make install
+cd ..
+tar cJf llvm-release.tar.xz llvm-release/
+
+# llvm-13.0.0-arm64-apple-darwin.tar.xz
